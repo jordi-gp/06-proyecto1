7142c372a51b9c1640ef67c2093741a8b8181985
Jordi García Pons <jordigar00@gmail.com>
2023-08-25 18:42:30 +0200

    Eliminacion de comentario en el componente Listado

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 
 export const Listado = ({listState, setListState}) => {
 
-  //const [listState, setListState] = useState([]);
+  const [listState, setListState] = useState([]);
 
   /* Función que se ejecuta una sola vez al cargar la página
   *  y que se encarga de obtener todas las peliculas del local
